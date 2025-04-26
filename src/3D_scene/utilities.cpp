f770e7c200ef252216a908645e667d8f8a4c4ba1
Noah Humphrey <thatnoahlh@gmail.com>
2025-04-25 19:51:18 -0700

    Update utilities.cpp

--- src/3D_scene/utilities.cpp
+++ src/3D_scene/utilities.cpp
@@ -1,1 +1,9 @@
-//util c
+// utilities.cpp
+//
+// This file contains utility functions for 3D rendering and transformations.
+
+#include "utilities.h"
+
+void renderScene() {
+    // Placeholder function for rendering the 3D scene
+}
