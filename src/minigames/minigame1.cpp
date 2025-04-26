6d518f657d86b35c674e46ecbb0a4be77935f58b
Noah Humphrey <thatnoahlh@gmail.com>
2025-04-25 19:49:39 -0700

    Update minigame1.cpp

--- src/minigames/minigame1.cpp
+++ src/minigames/minigame1.cpp
@@ -1,1 +1,11 @@
-//minigame1
+// minigame1.cpp
+//
+// This is the first minigame. Minigames are meant to enhance
+// the 3D environment and provide additional engagement for the user.
+
+#include <iostream>
+
+void playMinigame1() {
+    // Placeholder function for minigame logic
+    std::cout << "Minigame 1 is starting..." << std::endl;
+}
